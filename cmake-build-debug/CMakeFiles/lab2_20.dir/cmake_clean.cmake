9652f2e2301348519f48f0b1ef7af930f205298b
blizpear <blizpear@gmail.com>
2020-09-28 00:30:20 +0700

    Create cmake_clean.cmake

---
+++ cmake-build-debug/CMakeFiles/lab2_20.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2_20.dir/File.cpp.o"
+  "CMakeFiles/lab2_20.dir/main.cpp.o"
+  "lab2_20"
+  "lab2_20.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2_20.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
